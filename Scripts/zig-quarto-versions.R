f5a793fb77a23c6c93599a4a3e39ab8b55e37691
pedropark99 <pedropark99@gmail.com>
2024-06-13 20:11:26 -0300

    Add content

---
+++ Scripts/zig-quarto-versions.R
@@ -0,0 +1,54 @@
+library(stringr)
+
+options(zig_exe_path = "/home/pedro-dev/Documents/_Programs/zig-linux-x86_64-0.11.0/zig")
+
+find_zig_ <- function() {
+  p <- Sys.which("zig")
+  if (p == "") {
+    p <- getOption("zig_exe_path")
+  }
+  return(p)
+}
+
+find_quarto_ <- function() {
+  p <- Sys.which("quarto")
+  return(p)
+}
+
+zig_cmd_path <- find_zig_()
+quarto_cmd_path <- find_quarto_()
+zig_version <- system2(
+  zig_cmd_path,
+  "version",
+  stdout = TRUE
+)
+
+quarto_version <- system2(
+  quarto_cmd_path,
+  "-v",
+  stdout = TRUE
+)
+
+regex <- "[0-9]{2}[.][0-9]{2}[.][0-9]{1}-[a-zA-Z]*"
+sys_info <- Sys.info()
+sys_version <- str_extract(sys_info["version"], regex)
+bullets <- c(
+  "- System version: %s, %s, %s, %s.",
+  "- Zig version: %s.",
+  "- Quarto version: %s."
+)
+bullets <- paste(bullets, sep = "", collapse = "\n\n")
+versions_string <- sprintf(
+  bullets
+  , sys_info["sysname"]
+  , sys_info["release"]
+  , sys_version
+  , sys_info["machine"]
+  , zig_version
+  , quarto_version
+)
+
+
+
+
+cat(versions_string)
--- Tests/base64_basic.zig
+++ Tests/base64_basic.zig
@@ -6,39 +6,143 @@ fn print(input: []const u8) !void {
 }
 
 const Base64 = struct {
-    table: *const [64]u8,
+    _table: *const [64]u8,
 
     pub fn init() Base64 {
         const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         const lower = "abcdefghijklmnopqrstuvwxyz";
         const numbers = "0123456789+/";
         return Base64{
-            .table = upper ++ lower ++ numbers,
+            ._table = upper ++ lower ++ numbers,
         };
     }
 
-    pub fn char_at(self: Base64, index: u8) u8 {
-        return self.table[index];
+    fn _char_at(self: Base64, index: u8) u8 {
+        return self._table[index];
+    }
+
+    fn _char_index(self: Base64, char: u8) u8 {
+        if (char == '=')
+            return 64;
+
+        var index: u8 = 0;
+        for (0..63) |_| {
+            if (self._char_at(index) == char)
+                break;
+            index += 1;
+        }
+
+        return index;
+    }
+
+    fn encode(self: Base64, input: []const u8, allocator: std.mem.Allocator) ![]u8 {
+        if (input.len == 0)
+            return "";
+
+        const n_output = _calc_encode_length(input);
+        var output = try allocator.alloc(u8, n_output);
+        var tmp_buffer = [3]u8{ 0, 0, 0 };
+        var count: u8 = 0;
+        var output_index: u64 = 0;
+
+        for (input, 0..) |_, i| {
+            tmp_buffer[count] = input[i];
+            count += 1;
+            if (count == 3) {
+                output[output_index] = self._char_at(tmp_buffer[0] >> 2);
+                output[output_index + 1] = self._char_at(((tmp_buffer[0] & 0x03) << 4) + (tmp_buffer[1] >> 4));
+                output[output_index + 2] = self._char_at(((tmp_buffer[1] & 0x0f) << 2) + (tmp_buffer[2] >> 6));
+                output[output_index + 3] = self._char_at(tmp_buffer[2] & 0x3f);
+                output_index += 4;
+                count = 0;
+            }
+        }
+
+        if (count == 1) {
+            output[output_index] = self._char_at(tmp_buffer[0] >> 2);
+            output[output_index + 1] = self._char_at((tmp_buffer[0] & 0x03) << 4);
+            output[output_index + 2] = '=';
+            output[output_index + 3] = '=';
+        }
+
+        if (count == 2) {
+            output[output_index] = self._char_at(tmp_buffer[0] >> 2);
+            output[output_index + 1] = self._char_at(((tmp_buffer[0] & 0x03) << 4) + (tmp_buffer[1] >> 4));
+            output[output_index + 2] = self._char_at((tmp_buffer[1] & 0x0f) << 2);
+            output[output_index + 3] = '=';
+            output_index += 4;
+        }
+
+        return output;
+    }
+
+    fn decode(self: Base64, input: []const u8, allocator: std.mem.Allocator) ![]u8 {
+        if (input.len == 0)
+            return "";
+
+        const n_output = _calc_decode_length(input);
+        var tmp_buffer = [4]u8{ 0, 0, 0, 0 };
+        var output = try allocator.alloc(u8, n_output);
+        var count: u8 = 0;
+        var output_index: u64 = 0;
+
+        for (output, 0..) |_, i| {
+            // Initialize all fields to zero;
+            output[i] = 0;
+        }
+
+        for (input, 0..) |_, i| {
+            const index: u8 = @intCast(i);
+            tmp_buffer[count] = self._char_index(input[index]);
+            count += 1;
+            if (count == 4) {
+                output[output_index] = (tmp_buffer[0] << 2) + (tmp_buffer[1] >> 4);
+                if (tmp_buffer[2] != 64) {
+                    output[output_index + 1] = (tmp_buffer[1] << 4) + (tmp_buffer[2] >> 2);
+                }
+                if (tmp_buffer[3] != 64) {
+                    output[output_index + 2] = (tmp_buffer[2] << 6) + tmp_buffer[3];
+                }
+                output_index += 3;
+                count = 0;
+            }
+        }
+
+        return output;
+    }
+
+    fn _calc_encode_length(input: []const u8) u64 {
+        const len_as_float: f64 = @floatFromInt(input.len);
+        if (input.len < 3) {
+            const n_output: u64 = 4;
+            return n_output;
+        }
+
+        const n_output: u64 = @intFromFloat(@ceil(len_as_float / 3.0) * 4.0);
+        return n_output;
+    }
+
+    fn _calc_decode_length(input: []const u8) u64 {
+        const len_as_float: f64 = @floatFromInt(input.len);
+        if (input.len < 4) {
+            const n_output: u64 = 3;
+            return n_output;
+        }
+        const n_output: u64 = @intFromFloat(@floor(len_as_float / 4.0) * 3.0);
+        return n_output;
     }
 };
 
-fn add_padding_(input: []const u8) []u8 {
-    var bytes_with_padding = [3]u8{ 0, 0, 0 };
-    for (input, 0..) |_, i| {
-        bytes_with_padding[i] = input[i];
-        try stdout.print("{d} ", .{bytes_with_padding[i]});
-    }
-    return &bytes_with_padding;
-}
-
-fn decode(input: []const u8) !void {
-    if (input.len == 0) return;
-
-    const bytes_to_convert = add_padding_(input);
-    try print(bytes_to_convert);
-}
-
 pub fn main() !void {
-    const text = "Hi";
-    try decode(text);
+    var memory_buffer: [1000]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&memory_buffer);
+    const allocator = fba.allocator();
+
+    const text = "Testing some more shit";
+    const etext = "VGVzdGluZyBzb21lIG1vcmUgc2hpdA==";
+    const base64 = Base64.init();
+    const encoded_text = try base64.encode(text, allocator);
+    const decoded_text = try base64.decode(etext, allocator);
+    try stdout.print("Encoded text: {s}\n", .{encoded_text});
+    try stdout.print("Decoded text: {s}\n", .{decoded_text});
 }
