95af51b7e7b88b5807f6460a03aebd69b1d116b4
pedropark99 <pedropark99@gmail.com>
2024-06-22 17:11:31 -0300

    Add content

--- Tests/login_example.zig
+++ Tests/login_example.zig
@@ -2,13 +2,19 @@ const std = @import("std");
 const stdin = std.io.getStdIn();
 
 pub fn main() !void {
-    var input: [6]u8 = undefined;
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    var input = try allocator.alloc(u8, 50);
+    defer allocator.free(input);
+
+    for (0..input.len) |i| {
+        input[i] = 0; // initialize all fields to zero.
+    }
 
     const input_reader = stdin.reader();
     _ = try input_reader.readUntilDelimiterOrEof(
-        &input,
+        input,
         '\n'
     );
-
     std.debug.print("{s}\n", .{input});
 }
