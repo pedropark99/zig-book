4d293b4670e61465f6d06a7c9723a0a43c0adc5b
pedropark99 <pedropark99@gmail.com>
2024-06-29 16:06:35 -0300

    Add content

---
+++ ZigExamples/zig-basics/user_struct.zig
@@ -0,0 +1,15 @@
+const std = @import("std");
+const User = struct {
+    id: u64,
+    name: []const u8,
+    email: []const u8,
+
+    pub fn init(id: u64, name: []const u8, email: []const u8) User {
+        return User{ .id = id, .name = name, .email = email };
+    }
+};
+
+pub fn main() !void {
+    const u = User.init(1, "pedro", "pedrosomeemail@gmail.com");
+    _ = u;
+}
