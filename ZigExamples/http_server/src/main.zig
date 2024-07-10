6b9357c261150401b7916a01404bb846c4e20621
pedropark99 <pedropark99@gmail.com>
2024-07-10 19:24:20 -0300

    Add content

--- ZigExamples/http_server/src/main.zig
+++ ZigExamples/http_server/src/main.zig
@@ -8,7 +8,14 @@ pub fn main() !void {
     try stdout.print("Server Addr: {any}\n", .{socket._address});
     var server = try socket._address.listen(.{});
     const connection = try server.accept();
-    try Request.read_request(connection);
+
+    var buffer: [1000]u8 = undefined;
+    for (0..buffer.len) |i| {
+        buffer[i] = 0;
+    }
+    try Request.read_request(connection, buffer[0..buffer.len]);
+    const request = Request.parse_request(buffer[0..buffer.len]);
+    try stdout.print("{any}\n", .{request});
 }
 
 // const reader = incoming_connection.stream.reader();
--- ZigExamples/http_server/src/request.zig
+++ ZigExamples/http_server/src/request.zig
@@ -1,38 +1,62 @@
 const std = @import("std");
 const Connection = std.net.Server.Connection;
 
-pub fn read_request(conn: Connection) !void {
-    var buffer: [1000]u8 = undefined;
-    for (0..buffer.len) |i| {
-        buffer[i] = 0;
-    }
+pub fn read_request(conn: Connection, buffer: []u8) !void {
     const reader = conn.stream.reader();
-    _ = try reader.read(&buffer);
-    std.debug.print("{s}\n", .{buffer});
-    //parse_request(&buffer);
+    _ = try reader.read(buffer);
 }
 
-const Method = enum { GET, POST };
+const Map = std.static_string_map.StaticStringMap;
+const MethodMap = Map(Method).initComptime(.{
+    .{ "GET", Method.GET },
+});
+const Method = enum {
+    GET,
 
-const Header = struct {
-    version: []const u8,
-    method: Method,
-    uri: []const u8,
+    pub fn init(text: []const u8) !Method {
+        return MethodMap.get(text).?;
+    }
+
+    pub fn is_supported(m: []const u8) bool {
+        const method = MethodMap.get(m);
+        if (method) |_| {
+            return true;
+        }
+        return false;
+    }
 };
 
-fn parse_request(text: *[1000]u8) void {
-    var iterator = std.mem.splitScalar(u8, text, ' ');
-    var method = Method.GET;
-    var field = iterator.next();
-    method = parse_method(field);
-    std.debug.print("{any}\n", .{method});
-    field = iterator.next();
-    std.debug.print("{?s}\n", .{field});
+const Request = struct {
+    method: Method,
+    version: []const u8,
+    uri: []const u8,
+
+    pub fn init(method: Method, uri: []const u8, version: []const u8) Request {
+        return Request{
+            .method = method,
+            .uri = uri,
+            .version = version,
+        };
+    }
+};
+
+pub fn parse_request(text: []u8) Request {
+    const line_index = std.mem.indexOfScalar(u8, text, '\n') orelse text.len;
+    var iterator = std.mem.splitScalar(u8, text[0..line_index], ' ');
+    const method = try Method.init(iterator.next().?);
+    const uri = iterator.next().?;
+    const version = iterator.next().?;
+    const request = Request.init(method, uri, version);
+    return request;
 }
 
-fn parse_method(text: ?[]const u8) Method {
-    if (std.mem.eql(u8, text.?, "GET")) {
-        return Method.GET;
-    }
-    return Method.POST;
+test "teste" {
+    const a = "GET / HTTP/1.1\n";
+    var iterator = std.mem.splitScalar(u8, a, ' ');
+    try std.testing.expectEqualStrings("GET", iterator.next().?);
+    try std.testing.expectEqualStrings("/", iterator.next().?);
+    try std.testing.expectEqualStrings("HTTP/1.1\n", iterator.next().?);
+
+    const index = std.mem.indexOfScalar(u8, a, '\n');
+    try std.testing.expect(index == 14);
 }
