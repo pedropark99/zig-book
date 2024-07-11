3efdf889e21cdf9808346a25911064f7f4119fd7
pedropark99 <pedropark99@gmail.com>
2024-07-11 18:35:33 -0300

    Add content

--- ZigExamples/image_filter/build.zig
+++ ZigExamples/image_filter/build.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const LazyPath = std.Build.LazyPath;
 
 // Although this function looks imperative, note that its job is to
 // declaratively construct a build graph that will be executed by an external
@@ -15,26 +16,16 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "image_filter",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
-    b.installArtifact(lib);
-
     const exe = b.addExecutable(.{
         .name = "image_filter",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
+
+    exe.linkLibC();
+    exe.linkSystemLibrary("spng");
+    exe.addLibraryPath(LazyPath{ .cwd_relative = "/usr/local/lib/" });
 
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
@@ -64,16 +55,6 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
-    const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
     const exe_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
@@ -86,6 +67,5 @@ pub fn build(b: *std.Build) void {
     // the `zig build --help` menu, providing a way for the user to request
     // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
     test_step.dependOn(&run_exe_unit_tests.step);
 }
--- ZigExamples/image_filter/src/main.zig
+++ ZigExamples/image_filter/src/main.zig
@@ -4,20 +4,68 @@ const c = @cImport({
     @cInclude("stdio.h");
 });
 const png = @cImport({
-    @cInclude("png.h");
+    @cInclude("spng.h");
 });
 
 pub fn main() !void {
-    const file_path = "pedro_pascal.png";
-    const file = c.fopen(file_path, "rb");
-    if (file == null) {
-        @panic("Error at open! Probably file not found!");
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    var buffer = try allocator.alloc(u8, 289477);
+    defer allocator.free(buffer);
+    for (0..buffer.len) |i| {
+        buffer[i] = 0;
     }
-    const png_ptr = png.png_create_read_struct(png.PNG_LIBPNG_VER_STRING, null, null, null);
-    defer png.png_destroy_read_struct(&png_ptr, null, null);
-    const info_ptr = png.png_create_info_struct(png_ptr);
-    const png_transforms = png.PNG_TRANSFORM_STRIP_16;
-    png.png_init_io(png_ptr, file);
-    png.png_read_png(png_ptr, info_ptr, png_transforms, null);
-    _ = c.fclose(file);
+
+    var file = try std.fs.cwd().openFile("pedro_pascal.png", .{});
+    defer file.close();
+
+    var buf_reader = std.io.bufferedReader(file.reader());
+    var in_stream = buf_reader.reader();
+    const n_bytes = try in_stream.read(buffer[0..buffer.len]);
+    _ = n_bytes;
+
+    const ctx = png.spng_ctx_new(0);
+    defer png.spng_ctx_free(ctx);
+    _ = png.spng_set_png_buffer(ctx, @as([*c]u8, @ptrCast(buffer)), buffer.len);
+    var out_size: usize = 0;
+    _ = png.spng_decoded_image_size(ctx, png.SPNG_FMT_RGB8, &out_size);
+    std.debug.print("Image size: {d}\n", .{out_size});
+
+    var decoded_buffer = try allocator.alloc(u8, out_size);
+    defer allocator.free(decoded_buffer);
+    for (0..decoded_buffer.len) |i| {
+        decoded_buffer[i] = 0;
+    }
+
+    _ = png.spng_decode_image(ctx, @as([*c]u8, @ptrCast(decoded_buffer)), decoded_buffer.len, png.SPNG_FMT_RGB8, 0);
+    std.debug.print("Decoded: {any}\n", .{decoded_buffer[0..40]});
+
+    const n_pixels = 36000;
+    var cred: @Vector(n_pixels, u8) = undefined;
+    var cblue: @Vector(n_pixels, u8) = undefined;
+    var cgreen: @Vector(n_pixels, u8) = undefined;
+    var index: u64 = 0;
+    var count: u8 = 0;
+    for (0..decoded_buffer.len) |i| {
+        if (count == 0) {
+            std.debug.print("{d}\n", .{i});
+            cred[index] = decoded_buffer[i];
+            index += 1;
+            count += 1;
+            continue;
+        }
+        if (count == 1) {
+            cgreen[index] = decoded_buffer[i];
+            index += 1;
+            count += 1;
+            continue;
+        }
+
+        cblue[index] = decoded_buffer[i];
+        index += 1;
+        count = 0;
+    }
+
+    // var gray_buffer = try allocator.alloc(u8, n_pixels);
+    // defer allocator.free(gray_buffer);
 }
