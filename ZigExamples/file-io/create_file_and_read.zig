61a2ff62faee9bc145f1ffe294ac2b6cf063aaad
pedropark99 <pedropark99@gmail.com>
2024-08-22 21:34:22 -0300

    Fix file reading

--- ZigExamples/file-io/create_file_and_read.zig
+++ ZigExamples/file-io/create_file_and_read.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const stdout = std.io.getStdOut().writer();
 pub fn main() !void {
     const cwd = std.fs.cwd();
-    const file = try cwd.createFile("foo.txt", .{});
+    const file = try cwd.createFile("foo.txt", .{ .read = true });
     defer file.close();
 
     var fw = file.writer();
@@ -10,6 +10,7 @@ pub fn main() !void {
 
     var buffer: [300]u8 = undefined;
     @memset(buffer[0..], 0);
+    try file.seekTo(0);
     var fr = file.reader();
     _ = try fr.readAll(buffer[0..]);
     try stdout.print("{s}\n", .{buffer});
