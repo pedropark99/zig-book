372ecaa59162cfaf8747f20a826bc7b92bb8d10e
pedropark99 <pedropark99@gmail.com>
2024-08-31 16:45:34 -0300

    Add content

---
+++ ZigExamples/calling-c/fopen.zig
@@ -0,0 +1,13 @@
+const std = @import("std");
+const c = @cImport({
+    @cDefine("_NO_CRT_STDIO_INLINE", "1");
+    @cInclude("stdio.h");
+});
+
+pub fn main() !void {
+    const file = c.fopen("ZigExamples/image_filter/build.zig", "rb");
+    const close_status = c.fclose(file);
+    if (close_status != 0) {
+        return error.CouldNotCloseFileDescriptor;
+    }
+}
