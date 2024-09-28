7ff591165b1e4b60e51499c193f41453379400d9
pedropark99 <pedropark99@gmail.com>
2024-09-28 10:57:34 -0300

    Add content

--- ZigExamples/vectors/src/main.zig
+++ ZigExamples/vectors/src/main.zig
@@ -5,7 +5,7 @@ pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
     const Random = std.crypto.random;
-    const n: usize = 100;
+    const n: usize = 1000;
     var A: [n][]u64 = undefined;
     var B: [n][]u64 = undefined;
     var C: [n][]u64 = undefined;
@@ -13,45 +13,38 @@ pub fn main() !void {
         A[i] = try allocator.alloc(u64, n);
         B[i] = try allocator.alloc(u64, n);
         C[i] = try allocator.alloc(u64, n);
-        defer allocator.free(A[i]);
-        defer allocator.free(B[i]);
-        defer allocator.free(C[i]);
-
         for (0..n) |j| {
             const random_num = Random.int(u8);
-            A[i].ptr[j] = random_num;
-            B[i].ptr[j] = random_num;
-            C[i].ptr[j] = 0;
+            A[i][j] = random_num;
+            B[i][j] = random_num;
+            C[i][j] = 0;
         }
     }
 
-    try stdout.print("{any}\n", .{A[0].ptr[0]});
-    // try print_matrix(A, "A");
+    const nv: usize = 10;
+    var Aar: [nv]u64 = undefined;
+    var Bar: [nv]u64 = undefined;
+    for (0..n) |i| {
+        for (0..n) |j| {
+            var k: usize = 0;
+            while (k < n) : (k += nv) {
+                @memcpy(&Aar, A[i][k..(k + nv)]);
+                for (0..nv) |l| {
+                    Bar[l] = B[k + l][j];
+                }
+                const Av: @Vector(nv, u64) = Aar;
+                const Bv: @Vector(nv, u64) = Bar;
+                const result = Av * Bv;
+                C[i][j] += @reduce(.Add, result);
+            }
+        }
+    }
 
-    // var Aar: [10]u64 = undefined;
-    // var Bar: [10]u64 = undefined;
-    // for (0..n) |i| {
-    //     for (0..n) |j| {
-    //         var k: usize = 0;
-    //         while (k < n) : (k += 10) {
-    //             @memcpy(&Aar, A[i][k..(k + 10)]);
-    //             Bar[0] = B[k][j];
-    //             Bar[1] = B[k + 1][j];
-    //             Bar[2] = B[k + 2][j];
-    //             Bar[3] = B[k + 3][j];
-    //             Bar[4] = B[k + 4][j];
-    //             Bar[5] = B[k + 5][j];
-    //             Bar[6] = B[k + 6][j];
-    //             Bar[7] = B[k + 7][j];
-    //             Bar[8] = B[k + 8][j];
-    //             Bar[9] = B[k + 9][j];
-    //             const Av: @Vector(10, u64) = Aar;
-    //             const Bv: @Vector(10, u64) = Bar;
-    //             const result = Av * Bv;
-    //             C[i][j] += @reduce(.Add, result);
-    //         }
-    //     }
-    // }
+    for (0..n) |i| {
+        allocator.free(A[i]);
+        allocator.free(B[i]);
+        allocator.free(C[i]);
+    }
 }
 
 fn print_matrix(matrix: [100][]u64, name: []const u8) !void {
--- ZigExamples/vectors/src/main2.zig
+++ ZigExamples/vectors/src/main2.zig
@@ -2,13 +2,17 @@ const std = @import("std");
 const stdout = std.io.getStdOut().writer();
 
 pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
     const Random = std.crypto.random;
-    const n: usize = 100;
+    const n: usize = 1000;
     var A: [n][]u64 = undefined;
     var B: [n][]u64 = undefined;
     var C: [n][]u64 = undefined;
-
     for (0..n) |i| {
+        A[i] = try allocator.alloc(u64, n);
+        B[i] = try allocator.alloc(u64, n);
+        C[i] = try allocator.alloc(u64, n);
         for (0..n) |j| {
             const random_num = Random.int(u8);
             A[i][j] = random_num;
@@ -23,5 +27,11 @@ pub fn main() !void {
                 C[i][j] += A[i][k] * B[k][j];
             }
         }
+    }
+
+    for (0..n) |i| {
+        allocator.free(A[i]);
+        allocator.free(B[i]);
+        allocator.free(C[i]);
     }
 }
