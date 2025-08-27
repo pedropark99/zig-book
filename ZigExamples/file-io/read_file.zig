7b8d4dad0b2cd0c82ab647c0b81aef521b15e989
pedropark99 <pedropark99@gmail.com>
2025-08-27 20:30:13 -0300

    Fix stdout calls

--- ZigExamples/file-io/read_file.zig
+++ ZigExamples/file-io/read_file.zig
@@ -2,10 +2,18 @@ const std = @import("std");
 const builtin = @import("builtin");
 
 fn read_file(allocator: std.mem.Allocator, path: []const u8) ![]u8 {
+    var reader_buffer: [1024]u8 = undefined;
+    var file_buffer = try allocator.alloc(u8, 1024);
+    @memset(file_buffer[0..], 0);
+
     const file = try std.fs.cwd().openFile(path, .{});
     defer file.close();
 
-    return try file.reader().readAllAlloc(allocator, std.math.maxInt(usize));
+    var reader = file.reader(reader_buffer[0..]);
+    const nbytes = try reader.read(
+        file_buffer[0..]
+    );
+    return file_buffer[0..nbytes];
 }
 
 pub fn main() !void {
