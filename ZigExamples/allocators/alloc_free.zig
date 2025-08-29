a193c4d757b978ead4e9895b9d739b06a04bcd2c
pedropark99 <pedropark99@gmail.com>
2025-08-28 22:37:06 -0300

    Fix memory examples and buffered io section

---
+++ ZigExamples/allocators/alloc_free.zig
@@ -0,0 +1,22 @@
+const std = @import("std");
+var stdin_buffer: [1024]u8 = undefined;
+var stdin_reader = std.fs.File.stdin().reader(&stdin_buffer);
+const stdin = &stdin_reader.interface;
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    var input = try allocator.alloc(u8, 50);
+    defer allocator.free(input);
+    @memset(input[0..], 0);
+
+    // Read user input
+    stdin.readSliceAll(input[0..]) catch |err| switch(err) {
+        // Reached end of input, do nothing else
+        error.EndOfStream => {},
+        // If it's other kind of error, then return it
+        else => return err,
+    };
+    std.debug.print("{s}\n", .{input});
+}
+
