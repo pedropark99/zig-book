e1dcc006c542d21a0890a524aa549eb72e52c774
pedropark99 <pedropark99@gmail.com>
2025-11-15 14:37:58 -0300

    Fix broken paragraph

--- ZigExamples/file-io/read_file.zig
+++ ZigExamples/file-io/read_file.zig
@@ -2,14 +2,13 @@ const std = @import("std");
 const builtin = @import("builtin");
 
 fn read_file(allocator: std.mem.Allocator, path: []const u8) ![]u8 {
-    var reader_buffer: [1024]u8 = undefined;
     var file_buffer = try allocator.alloc(u8, 1024);
     @memset(file_buffer[0..], 0);
 
     const file = try std.fs.cwd().openFile(path, .{});
     defer file.close();
 
-    var reader = file.reader(reader_buffer[0..]);
+    var reader = file.reader();
     const nbytes = try reader.read(
         file_buffer[0..]
     );
@@ -22,5 +21,6 @@ pub fn main() !void {
     const path = "./ZigExamples/file-io/shop-list.txt";
     const file_contents = try read_file(allocator, path);
     const slice = file_contents[0..file_contents.len];
-    _ = slice;
+
+    std.debug.print("{s}", .{slice});
 }
