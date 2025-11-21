421e76c41aa13b39ecdcf0903194ed78a3ce9a13
pedropark99 <pedropark99@gmail.com>
2025-11-21 16:27:18 -0300

    File IO to Zig 0.16

--- ZigExamples/file-io/create_file_and_read.zig
+++ ZigExamples/file-io/create_file_and_read.zig
@@ -1,17 +1,30 @@
 const std = @import("std");
-const stdout = std.io.getStdOut().writer();
+var stdout_buffer: [1024]u8 = undefined;
+var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
+const stdout = &stdout_writer.interface;
+
 pub fn main() !void {
     const cwd = std.fs.cwd();
-    const file = try cwd.createFile("foo.txt", .{ .read = true });
+    const io = std.testing.io;
+    const file = try cwd.createFile(
+        "foo.txt",
+        .{ .read = true }
+    );
     defer file.close();
-
-    var fw = file.writer();
-    _ = try fw.writeAll("We are going to read this line\n");
+    _ = try file.writeAll(
+        "We are going to read this line\n"
+    );
 
     var buffer: [300]u8 = undefined;
     @memset(buffer[0..], 0);
+
     try file.seekTo(0);
-    var fr = file.reader();
-    _ = try fr.readAll(buffer[0..]);
+    var read_buffer: [1024]u8 = undefined;
+    var fr = file.reader(io, &read_buffer);
+    var reader = &fr.interface;
+
+    _ = reader.readSliceAll(buffer[0..]) catch 0;
+
     try stdout.print("{s}\n", .{buffer});
+    try stdout.flush();
 }
