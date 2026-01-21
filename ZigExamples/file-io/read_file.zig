b2aae11e1fd569834e0ab2ee3cfd03ec2df5bf0c
pedropark99 <pedropark99@gmail.com>
2026-01-21 09:35:51 -0300

    Fix read file memory leak

--- ZigExamples/file-io/read_file.zig
+++ ZigExamples/file-io/read_file.zig
@@ -1,23 +1,25 @@
 const std = @import("std");
 const builtin = @import("builtin");
 
-fn read_file(allocator: std.mem.Allocator, path: []const u8) ![]u8 {
+fn read_file(path: []const u8, buffer: []u8) !usize {
     const file = try std.fs.cwd().openFile(path, .{});
     defer file.close();
 
-    var file_buffer = try allocator.alloc(u8, 1024);
-    @memset(file_buffer[0..], 0);
-
-    const nbytes = try file.read(file_buffer[0..]);
-    return file_buffer[0..nbytes];
+    return try file.read(buffer[0..]);
 }
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
     const allocator = gpa.allocator();
-    const path = "./ZigExamples/file-io/shop-list.txt";
-    const file_contents = try read_file(allocator, path);
-    const slice = file_contents[0..file_contents.len];
 
-    std.debug.print("{s}", .{slice});
+    var file_buffer = try allocator.alloc(u8, 1024);
+    defer allocator.free(file_buffer);
+    @memset(file_buffer[0..], 0);
+
+    const path = "./ZigExamples/file-io/shop-list.txt";
+    const nbytes = try read_file(path, file_buffer[0..]);
+
+    std.debug.print("{s}", .{file_buffer[0..nbytes]});
 }
+
