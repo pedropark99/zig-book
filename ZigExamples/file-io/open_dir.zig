2835d6d3c8ac9468d30d8dfc12a6452e888ecf8f
pedropark99 <pedropark99@gmail.com>
2026-02-28 18:01:22 -0300

    Chapter file IO fixed

---
+++ ZigExamples/file-io/open_dir.zig
@@ -0,0 +1,21 @@
+const std = @import("std");
+
+pub fn main(init: std.process.Init) !void {
+    var stdout_buffer: [1024]u8 = undefined;
+    var stdout_writer = std.Io.File.stdout().writer(init.io, &stdout_buffer);
+    const stdout = &stdout_writer.interface;
+    const cwd = std.Io.Dir.cwd();
+    const dir = try cwd.openDir(
+        init.io,
+        "ZigExamples/file-io/",
+        .{ .iterate = true }
+    );
+    var it = dir.iterate();
+    while (try it.next(init.io)) |entry| {
+        try stdout.print(
+            "File name: {s}\n",
+            .{entry.name}
+        );
+    }
+    try stdout.flush();
+}
