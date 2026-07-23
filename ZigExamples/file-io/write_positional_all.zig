a8b1cfb25fef85e69083542c9693e12cde44c789
pedropark99 <pedropark99@gmail.com>
2026-07-22 21:49:48 -0300

    Mention Windows specific detail

---
+++ ZigExamples/file-io/write_positional_all.zig
@@ -0,0 +1,12 @@
+const std = @import("std");
+
+pub fn main(init: std.process.Init) !void {
+    const cwd = std.Io.Dir.cwd();
+    const file = try cwd.openFile(init.io, "foo.txt", .{ .mode = .read_write });
+    defer file.close(init.io);
+
+    const length = try file.length(init.io);
+    _ = try file.writePositionalAll(
+        init.io, "Some random text to write\n", length
+    );
+}
