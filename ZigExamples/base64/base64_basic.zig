c0e2686de004c8022606260257336ac44a2a0097
pedropark99 <pedropark99@gmail.com>
2026-07-25 11:27:15 -0300

    Add new diagram for Bas64 decoder logic

--- ZigExamples/base64/base64_basic.zig
+++ ZigExamples/base64/base64_basic.zig
@@ -152,6 +152,5 @@ pub fn main(init: std.process.Init) !void {
     try stdout.print("Decoded text: {s}\n", .{decoded_text});
     try stdout.print("Encoded length: {d}\n", .{encoded_text.len});
     try stdout.print("Decoded length: {d}\n", .{decoded_text.len});
-    try stdout.print("Index: {b}\n", .{36});
     try stdout.flush();
 }
