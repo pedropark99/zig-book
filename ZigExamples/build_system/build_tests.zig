cf8e6e99b23a71d17496aa7452649b351244d2f9
noderik <noderik@proton.me>
2026-09-20 14:26:05 +0200

    Update build_tests.zig, so it corresponds with the book.

--- ZigExamples/build_system/build_tests.zig
+++ ZigExamples/build_system/build_tests.zig
@@ -2,10 +2,14 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const test_exe = b.addTest(.{
         .name = "unit_tests",
-        .root_source_file = b.path("src/main.zig"),
-        .target = b.host,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = b.graph.host
+        })
     });
+
     b.installArtifact(test_exe);
+
     const run_arti = b.addRunArtifact(test_exe);
     const run_step = b.step("tests", "Run unit tests");
     run_step.dependOn(&run_arti.step);
